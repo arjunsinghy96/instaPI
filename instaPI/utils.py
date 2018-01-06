bdc0fcb1f534c16c62020744ecb69f2226fb5225
arjunsinghy96 <14bcs010@smvdu.ac.in>
2018-01-06 07:21:07 +0530

    Initial Commit

---
+++ instaPI/utils.py
@@ -0,0 +1,24 @@
+import requests
+from urllib.parse import urljoin
+import json
+
+BASE_URL = 'https://api.instagram.com/v1'
+
+def get_response_data(method):
+    """
+    Extracts the value of 'data' key from the response of request.
+    Raises appropriate error in case of any
+    """
+    def wrapper(*args, **kwargs):
+        response = method(*args, **kwargs)
+        data = json.loads(response.text)['data']
+        return data
+
+    return wrapper
+
+
+@get_response_data
+def get(access_token, path):
+    url =  '%s%s?access_token=%s' % (BASE_URL, path, access_token)
+    response = requests.get(url)
+    return response
